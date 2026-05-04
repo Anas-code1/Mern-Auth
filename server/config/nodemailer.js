1d083a6535d8380748218e819992a1980426bd13
Anas-code1 <j79729091@gmail.com>
2026-05-04 20:18:36 +0500

    resolved the render-brevo freeze

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -2,11 +2,14 @@ import nodemailer from 'nodemailer'
 
 const transporter = nodemailer.createTransport({
     host: 'smtp-relay.brevo.com',
-    port: 465,          // Change to 465
-    secure: true,       // MUST be true when using port 465
+    port: 587,         // Switch back to 587
+    secure: false,     // Must be false for 587
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS
+    },
+    tls: {
+        rejectUnauthorized: false // <-- THIS IS THE MAGIC FIX FOR RENDER
     }
 });
 
